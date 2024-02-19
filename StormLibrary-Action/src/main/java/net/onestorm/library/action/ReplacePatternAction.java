c353c258be175c56ef786c3adaca204f5e760c4a
CdeJong <christiaan240@gmail.com>
2024-02-20 00:20:27 +0100

    Removed Action placeholder interface

--- StormLibrary-Action/src/main/java/net/onestorm/library/action/ReplacePatternAction.java
+++
@@ -1,9 +0,0 @@
-package net.onestorm.library.action;
-
-import java.util.regex.Pattern;
-
-public interface ReplacePatternAction {
-
-    void replace(Pattern pattern, String value);
-
-}
--- StormLibrary-Action/src/main/java/net/onestorm/library/action/implementation/MessageAction.java
+++ StormLibrary-Action/src/main/java/net/onestorm/library/action/implementation/MessageAction.java
@@ -1,30 +1,16 @@
 package net.onestorm.library.action.implementation;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextReplacementConfig;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.onestorm.library.action.Action;
-import net.onestorm.library.action.ReplacePatternAction;
 import net.onestorm.library.user.User;
 
-import java.util.regex.Pattern;
 
-public class MessageAction implements Action, ReplacePatternAction {
+public class MessageAction implements Action {
 
-    private Component message;
+    private final Component message;
 
     public MessageAction(Component message) {
         this.message = message;
-    }
-
-    @Override
-    public void replace(Pattern pattern, String value) {
-        TextReplacementConfig configuration = TextReplacementConfig.builder()
-                .match(pattern)
-                .replacement(value)
-                .build();
-
-        message = message.replaceText(configuration);
     }
 
     @Override
