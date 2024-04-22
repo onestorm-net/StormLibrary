d3177506b46227d013291796681b2697070810b8
CdeJong <christiaan240@gmail.com>
2024-04-22 02:07:58 +0200

    Fixed slot not taking parameters

--- StormLibrary-Menu/src/main/java/net/onestorm/library/menu/element/Element.java
+++ StormLibrary-Menu/src/main/java/net/onestorm/library/menu/element/Element.java
@@ -2,14 +2,12 @@ package net.onestorm.library.menu.element;
 
 import net.onestorm.library.menu.slot.Slot;
 
-import java.util.Optional;
-
 
 public interface Element {
 
     String getName();
 
-    Slot<?> getSlot(int index);
+    Slot getSlot(int index);
 
     Iterable<Integer> getSlotIndexes();
 
