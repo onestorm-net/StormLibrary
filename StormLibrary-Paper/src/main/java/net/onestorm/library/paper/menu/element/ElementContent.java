2841e2de34f7da0e2b37fd3e9eb4c5500cafb6c1
CdeJong <christiaan240@gmail.com>
2024-05-08 03:47:13 +0200

    Fixed wrong implementation type

--- StormLibrary-Paper/src/main/java/net/onestorm/library/paper/menu/element/ElementContent.java
+++ StormLibrary-Paper/src/main/java/net/onestorm/library/paper/menu/element/ElementContent.java
@@ -11,26 +11,28 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class ElementContent implements Iterable<ItemStackCell> {
+public class ElementContent implements Iterable<Cell> {
 
     private final Menu menu;
-    private final Map<Integer, ItemStackCell> cellMap = new HashMap<>();
+    private final Map<Integer, Cell> cellMap = new HashMap<>();
 
     public ElementContent(Menu menu) {
         this.menu = menu;
     }
 
-    public Cell setItem(int index, ItemStack itemStack) {
-        ItemStackCell cell = cellMap.get(index);
+    public ItemStackCell setItem(int index, ItemStack itemStack) {
+        ItemStackCell result;
+        Cell cell = cellMap.get(index);
 
-        if (cell == null) {
-            cell = new ItemStackCellImpl(menu, index, itemStack);
-            cellMap.put(index, cell);
+        if (!(cell instanceof ItemStackCell itemStackCell)) {
+            result = new ItemStackCellImpl(menu, index, itemStack);
+            cellMap.put(index, result);
         } else {
-            cell.setItemStack(itemStack);
+            result = itemStackCell;
+            itemStackCell.setItemStack(itemStack);
         }
 
-        return cell;
+        return result;
     }
 
     public Cell setItem(int x, int y, ItemStack itemStack) {
@@ -44,7 +46,7 @@ public class ElementContent implements Iterable<ItemStackCell> {
 
     @NotNull
     @Override
-    public Iterator<ItemStackCell> iterator() {
+    public Iterator<Cell> iterator() {
         return cellMap.values().iterator();
     }
 }
