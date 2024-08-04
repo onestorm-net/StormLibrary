6b8f2419aca86329fa789a43fefe33bd015040ba
CdeJong <christiaan240@gmail.com>
2024-08-04 02:02:11 +0200

    Added access to ElementFactory

--- StormLibrary-Menu/src/main/java/net/onestorm/library/menu/build/MenuBuilder.java
+++ StormLibrary-Menu/src/main/java/net/onestorm/library/menu/build/MenuBuilder.java
@@ -1,12 +1,13 @@
 package net.onestorm.library.menu.build;
 
 import net.onestorm.library.menu.Menu;
+import net.onestorm.library.menu.element.build.ElementFactory;
 import net.onestorm.library.storage.StorageMap;
 
 public interface MenuBuilder {
 
     String getName();
 
-    Menu build(StorageMap configuration);
+    Menu build(ElementFactory elementFactory, StorageMap configuration);
 
 }
