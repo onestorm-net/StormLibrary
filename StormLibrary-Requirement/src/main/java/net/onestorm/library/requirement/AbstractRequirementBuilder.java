cbce3166aae59f7f45d2c4415468b34abcb04a66
CdeJong <christiaan240@gmail.com>
2024-08-04 03:55:22 +0200

    Updated documentation

--- StormLibrary-Requirement/src/main/java/net/onestorm/library/requirement/AbstractRequirementBuilder.java
+++ StormLibrary-Requirement/src/main/java/net/onestorm/library/requirement/AbstractRequirementBuilder.java
@@ -1,5 +1,6 @@
 package net.onestorm.library.requirement;
 
+import net.onestorm.library.common.factory.Builder;
 import net.onestorm.library.storage.StorageMap;
 
 public abstract class AbstractRequirementBuilder implements Builder<Requirement> {
@@ -14,7 +15,7 @@ public abstract class AbstractRequirementBuilder implements Builder<Requirement>
 
     /**
      * Builds a requirement from configuration section.
-     * @param configuration The configuration section.
+     * @param storage The configuration section.
      * @param isReverted Reverts the result from the build requirement when checked.
      * @return The requirement.
      */
