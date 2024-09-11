504ecdba8bc51433f35319b1d01c4636ee3e1e60
CdeJong <christiaan240@gmail.com>
2024-09-11 16:42:31 +0200

    Updated getMessage to allow multiple keys (varargs)

--- StormLibrary-Message/src/main/java/net/onestorm/library/message/MessageManager.java
+++ StormLibrary-Message/src/main/java/net/onestorm/library/message/MessageManager.java
@@ -3,11 +3,16 @@ package net.onestorm.library.message;
 public interface MessageManager {
 
     /**
-     * Retrieves a {@link MessageBuilder} for the specified message key.
+     * Retrieves a {@link MessageBuilder} for the first available message key from the provided list of keys.
+     * If multiple keys are supplied, it will return the {@link MessageBuilder} associated with the first key
+     * that has a corresponding message. If none of the keys are found, it returns an empty {@link MessageBuilder}
+     * (i.e., a {@link MessageBuilder} with no message set).
      *
-     * @param key the key identifying the message template or entry
-     * @return a {@link MessageBuilder} instance for building and formatting the message
+     * @param keys one or more keys identifying the message templates or entries to search for
+     * @return a {@link MessageBuilder} instance for the first key that exists,
+     * or an empty {@link MessageBuilder} if no corresponding messages are found for any of the provided keys
+     * @throws IllegalArgumentException if no keys are provided
      */
-    MessageBuilder getMessage(String key);
+    MessageBuilder getMessage(String... keys);
 
 }
