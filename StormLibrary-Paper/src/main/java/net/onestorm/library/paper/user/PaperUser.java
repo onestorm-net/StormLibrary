1a29217580970bd5f8742627e0973798eea7295d
CdeJong <christiaan240@gmail.com>
2025-08-29 01:04:24 +0200

    Changed PaperUser into the implementation (previously an interface)

--- StormLibrary-Paper/src/main/java/net/onestorm/library/paper/user/PaperUser.java
+++ StormLibrary-Paper/src/main/java/net/onestorm/library/paper/user/PaperUser.java
@@ -1,10 +1,40 @@
 package net.onestorm.library.paper.user;
 
+import net.kyori.adventure.text.Component;
 import net.onestorm.library.user.User;
 import org.bukkit.entity.Player;
 
-public interface PaperUser extends User {
+import java.util.UUID;
 
-    Player asPlayer();
+public class PaperUser implements User {
 
+    private final Player player;
+
+    public PaperUser(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public UUID getUuid() {
+        return player.getUniqueId();
+    }
+
+    @Override
+    public String getUsername() {
+        return player.getName();
+    }
+
+    @Override
+    public void sendMessage(Component message) {
+        player.sendMessage(message);
+    }
+
+    @Override
+    public boolean hasPermission(String node) {
+        return player.hasPermission(node);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
