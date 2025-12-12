8fec01622976a9bacd539aa8a42e84de812fdcd0
CdeJong <christiaan240@gmail.com>
2025-12-12 23:31:16 +0100

    Added documentation for cost api

--- StormLibrary-Cost/src/main/java/net/onestorm/library/cost/Cost.java
+++ StormLibrary-Cost/src/main/java/net/onestorm/library/cost/Cost.java
@@ -5,10 +5,28 @@ import net.onestorm.library.common.context.UserContext;
 
 public interface Cost {
 
+    /**
+     * Returns the identifier for this cost.
+     *
+     * @return the unique name of this cost
+     */
     String getName();
 
-    boolean canPay(UserContext user);
+    /**
+     * Determines whether the user represented by the given context is able to pay this cost.
+     *
+     * @param context the user context in which the payment is evaluated
+     * @return {@code true} if the user can pay this cost; {@code false} otherwise
+     */
+    boolean canPay(UserContext context);
 
-    Refundable pay(UserContext user);
+    /**
+     * Deducts this cost from the user represented by the given context.
+     *
+     * @param context the user context in which the payment is processed
+     * @return a {@link Refundable} representing the applied payment, used for potential rollback
+     * @throws RuntimeException if the cost cannot be paid for any reason
+     */
+    Refundable pay(UserContext context);
 
 }
--- StormLibrary-Cost/src/main/java/net/onestorm/library/cost/Refundable.java
+++ StormLibrary-Cost/src/main/java/net/onestorm/library/cost/Refundable.java
@@ -4,6 +4,12 @@ import net.onestorm.library.common.context.UserContext;
 
 public interface Refundable {
 
+    /**
+     * Reverts the previously applied payment associated with this {@code Refundable}.
+     *
+     * @param context the user context in which the refund is applied
+     * @throws RuntimeException if the refund cannot be completed
+     */
     void refund(UserContext context);
 
 }
